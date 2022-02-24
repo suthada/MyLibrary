0d5d3105bb80fed4c52007bfbad8a0dafcb4a84d
suthada <96216472+suthada@users.noreply.github.com>
2022-02-25 03:09:18 +0700

    ML

---
+++ MyLibrary/MyLibrary/MyMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    internal class MyMath
+    {
+        int getSummation(int a, int b)
+        {
+            return a+b;
+        }
+        int getSummation(int a, int b, int c)
+        {   
+            return a+b+c; 
+        }
+    }
+}
